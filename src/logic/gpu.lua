6dbeeab5a2e8490d8a852d9fe0f1f7f1d944e502
MegadreamsBE <sleeubusjordy@gmail.com>
2021-07-08 09:58:41 -0400

    Fix DMG sprite flipping

--- src/logic/gpu.lua
+++ src/logic/gpu.lua
@@ -370,8 +370,8 @@ function renderSprites()
             local tile = mmuReadByte(0xFE00 + index + 2)
             local attributes = mmuReadByte(0xFE00 + index + 3)
 
-            local yFlip = false
-            local xFlip = false
+            local yFlip = (_bitExtract(attributes, 6, 1) == 1)
+            local xFlip = (_bitExtract(attributes, 5, 1) == 1)
             local line = scanLine - yPos
 
             local cgbPalette = 0
